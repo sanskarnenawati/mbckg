4cedce60ffa47a3d805b5ed041b74f07c3f198da
Sanskar Nenawati <98727743+sanskarnenawati@users.noreply.github.com>
2023-04-29 08:19:47 +0530

    Add files via upload

---
+++ predict_cost.py
@@ -0,0 +1,10 @@
+import pickle
+
+
+def predict(data):
+    loaded_model = pickle.load(open("C:/Users/sansk/PycharmProjects/deployed/trained_model.sav", 'rb'))
+    return loaded_model.predict(data)
+    rt = joblib.load('C:/Users/sansk/PycharmProjects/deployed/label_encoder.sav')
+    return lr.predict(data)
+    t = joblib.load('C:/Users/sansk/PycharmProjects/deployed/scaler.sav')
+    return lr.predict(data)

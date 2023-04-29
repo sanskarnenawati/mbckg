212394208e2851eacfb9a9ebfaae211202c06761
Sanskar Nenawati <98727743+sanskarnenawati@users.noreply.github.com>
2023-04-29 08:30:03 +0530

    Update predict_cost.py

--- predict_cost.py
+++ predict_cost.py
@@ -2,9 +2,9 @@ import pickle
 
 
 def predict(data):
-    loaded_model = pickle.load(open("C:/Users/sansk/PycharmProjects/deployed/rentpred.sav", 'rb'))
+    loaded_model = pickle.load(open("rentpred.sav", 'rb'))
     return loaded_model.predict(data)
-    rt = joblib.load('C:/Users/sansk/PycharmProjects/deployed/label_encoder.sav')
+    rt = joblib.load('label_encoder.sav')
     return lr.predict(data)
-    t = joblib.load('C:/Users/sansk/PycharmProjects/deployed/scaler.sav')
+    t = joblib.load('scaler.sav')
     return lr.predict(data)
